f13ced93ff16e1c3968bd3f72e473f616ebe0e7c
Chris Cassano <chris@litprotocol.com>
2024-12-09 14:53:08 -0800

    yay it works

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ![Image](./header.png)
 
+# Lit Precompile Notes
+
+Build using `build.sh`, and then you can deploy using `cargo stylus deploy`. You may have to run `cargo clean` first, to remove any old builds.
+
 # Stylus Hello World
 
 Project starter template for writing Arbitrum Stylus programs in Rust using the [stylus-sdk](https://github.com/OffchainLabs/stylus-sdk-rs). It includes a Rust implementation of a basic counter Ethereum smart contract:
@@ -23,7 +27,7 @@ contract Counter {
 
 To set up more minimal example that still uses the Stylus SDK, use `cargo stylus new --minimal <YOUR_PROJECT_NAME>` under [OffchainLabs/cargo-stylus](https://github.com/OffchainLabs/cargo-stylus).
 
-## Quick Start 
+## Quick Start
 
 Install [Rust](https://www.rust-lang.org/tools/install), and then install the Stylus CLI tool with Cargo
 
@@ -117,7 +121,6 @@ Estimated gas for deployment: 1874876
 ```
 
 The above only estimates gas for the deployment tx by default. To estimate gas for activation, first deploy your program using `--mode=deploy-only`, and then run `cargo stylus deploy` with the `--estimate-gas-only` flag, `--mode=activate-only`, and specify `--activate-program-address`.
-
 
 Here's how to deploy:
 
--- src/lib.rs
+++ src/lib.rs
@@ -31,35 +31,6 @@ use stylus_sdk::{alloy_primitives::*, prelude::*};
 use hd_keys_curves_wasm::{EcCurve, HDDeriver};
 use k256::elliptic_curve::sec1::{FromEncodedPoint, ToEncodedPoint};
 
-//TODO: not sure if this part is necessary
-sol_interface! {
-    interface EcOps {
-        function hdKeyDerive(bytes32 curve, bytes points) external pure returns (bytes);
-        function ecMul(bytes32 curve, bytes point, bytes scalar) external pure returns (bytes);
-        function ecAdd(bytes32 curve, bytes point1, bytes point2) external pure returns (bytes);
-        function ecNeg(bytes32 curve, bytes point) external pure returns (bytes);
-        function ecEqual(bytes32 curve, bytes point1, bytes point2) external pure returns (bytes1);
-        function ecIsInf(bytes32 curve, bytes point) external pure returns (bytes1);
-        function ecIsValid(bytes32 curve, bytes point) external pure returns (bytes1);
-        function ecHash(bytes32 curve, bytes input) external pure returns (bytes);
-        function ecPairing(bytes32 curve, uint256 len, bytes points) external pure returns (bytes1);
-        function scAdd(bytes32 curve, bytes scalar1, bytes scalar2) external pure returns (bytes);
-        function scMul(bytes32 curve, bytes scalar1, bytes scalar2) external pure returns (bytes);
-        function scNeg(bytes32 curve, bytes scalar) external pure returns (bytes);
-        function scInv(bytes32 curve, bytes scalar) external pure returns (bytes);
-        function scSqrt(bytes32 curve, bytes scalar) external pure returns (bytes);
-        function scEqual(bytes32 curve, bytes scalar1, bytes scalar2) external pure returns (bytes1);
-        function scIsZero(bytes32 curve, bytes scalar) external pure returns (bytes1);
-        function scIsValid(bytes32 curve, bytes scalar) external pure returns (bytes1);
-        function scFromWideBytes(bytes32 curve, bytes input) external pure returns (bytes);
-        function scHash(bytes32 curve, uint256 len, bytes input) external pure returns (bytes);
-        function ecdsaVerify(bytes32 curve, bytes32 msg, bytes pubKey, bytes sig) external pure returns (bytes1);
-        function schnorrVerify1(bytes32 curve, bytes32 hash, bytes32 hashedMessage, bytes pubKey, bytes sig) external pure returns (bytes1);
-        function schnorrVerify2(bytes32 curve, bytes32 hash, bytes32 hashedMessage, bytes pubKey, bytes sig) external pure returns (bytes1);
-        function blsVerify(bytes32 curve, uint256 len, bytes msg, bytes pubKey, bytes sig) external pure returns (bytes1);
-    }
-}
-
 // Define some persistent storage using the Solidity ABI.
 // `Counter` will be the entrypoint.
 sol_storage! {
@@ -78,37 +49,37 @@ impl EcOpsContract {
         // 6th arg is a 4 byte big-endian integer for the number of root keys
         // 7th arg is a variable number of root keys each 64 bytes in length
         match data[0] {
-            0 => {
-                let id_len = u32::from_be_bytes(data[1..5].try_into().unwrap()) as usize;
-                let id = &data[5..5 + id_len];
-                let cxt_len =
-                    u32::from_be_bytes(data[5 + id_len..9 + id_len].try_into().unwrap()) as usize;
-                let cxt = &data[9 + id_len..9 + id_len + cxt_len];
-                let root_keys_len = u32::from_be_bytes(
-                    data[9 + id_len + cxt_len..13 + id_len + cxt_len]
-                        .try_into()
-                        .unwrap(),
-                ) as usize;
-                if root_keys_len % 64 != 0 {
-                    return Err(Vec::new());
-                }
-                let root_keys = &data[13 + id_len + cxt_len..13 + id_len + cxt_len + root_keys_len];
-                let mut hd_root_keys = vec![p256::ProjectivePoint::IDENTITY; root_keys_len];
-                for (hd_i, chunk) in hd_root_keys.iter_mut().zip(root_keys.chunks(64)) {
-                    let mut bytes = [4u8; 65];
-                    bytes[1..].copy_from_slice(chunk);
-                    let encoded_point =
-                        p256::EncodedPoint::from_bytes(bytes).map_err(|_| Vec::new())?;
+            // 0 => {
+            //     let id_len = u32::from_be_bytes(data[1..5].try_into().unwrap()) as usize;
+            //     let id = &data[5..5 + id_len];
+            //     let cxt_len =
+            //         u32::from_be_bytes(data[5 + id_len..9 + id_len].try_into().unwrap()) as usize;
+            //     let cxt = &data[9 + id_len..9 + id_len + cxt_len];
+            //     let root_keys_len = u32::from_be_bytes(
+            //         data[9 + id_len + cxt_len..13 + id_len + cxt_len]
+            //             .try_into()
+            //             .unwrap(),
+            //     ) as usize;
+            //     if root_keys_len % 64 != 0 {
+            //         return Err(Vec::new());
+            //     }
+            //     let root_keys = &data[13 + id_len + cxt_len..13 + id_len + cxt_len + root_keys_len];
+            //     let mut hd_root_keys = vec![p256::ProjectivePoint::IDENTITY; root_keys_len];
+            //     for (hd_i, chunk) in hd_root_keys.iter_mut().zip(root_keys.chunks(64)) {
+            //         let mut bytes = [4u8; 65];
+            //         bytes[1..].copy_from_slice(chunk);
+            //         let encoded_point =
+            //             p256::EncodedPoint::from_bytes(bytes).map_err(|_| Vec::new())?;
 
-                    let hd_key = p256::AffinePoint::from_encoded_point(&encoded_point)
-                        .map(p256::ProjectivePoint::from);
-                    *hd_i = Option::<p256::ProjectivePoint>::from(hd_key).ok_or(Vec::new())?;
-                }
-                let hd_deriver = <p256::Scalar as HDDeriver>::create(id, cxt);
-                let hd_key =
-                    hd_deriver.hd_derive_public_key::<p256::ProjectivePoint>(&hd_root_keys);
-                Ok(hd_key.to_encoded_point(false).as_bytes()[1..].to_vec())
-            }
+            //         let hd_key = p256::AffinePoint::from_encoded_point(&encoded_point)
+            //             .map(p256::ProjectivePoint::from);
+            //         *hd_i = Option::<p256::ProjectivePoint>::from(hd_key).ok_or(Vec::new())?;
+            //     }
+            //     let hd_deriver = <p256::Scalar as HDDeriver>::create(id, cxt);
+            //     let hd_key =
+            //         hd_deriver.hd_derive_public_key::<p256::ProjectivePoint>(&hd_root_keys);
+            //     Ok(hd_key.to_encoded_point(false).as_bytes()[1..].to_vec())
+            // }
             1 => {
                 let id_len = u32::from_be_bytes(data[1..5].try_into().unwrap()) as usize;
                 let id = &data[5..5 + id_len];
