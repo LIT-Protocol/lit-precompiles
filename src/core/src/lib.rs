5d0299e29d6f3ad96ea34068e9f153b33054dc07
Michael Lodder <mike@litprotocol.com>
2025-03-03 14:21:56 -0700

    remove extra files
    Signed-off-by: Michael Lodder <mike@litprotocol.com>

--- src/core/src/lib.rs
+++ src/core/src/lib.rs
@@ -1,9 +1,4 @@
 pub mod contract_traits;
-// #[cfg(feature = "k256")]
-// pub mod k256;
-// #[cfg(feature = "p256")]
-// pub mod p256;
-// pub mod utils;
 
 #[cfg(any(
     feature = "k256",
@@ -36,7 +31,7 @@ pub fn hd_key_derive(data: Vec<u8>) -> Result<Vec<u8>, Vec<u8>> {
     // 6th arg is a 4 byte big-endian integer for the number of root keys
     // 7th arg is a variable number of root keys each 64 bytes in length
     let curve = data[0];
-    let (id_len, data) = try_read_u32(&data[..])?;
+    let (id_len, data) = try_read_u32(&data[1..])?;
     let id = &data[..id_len];
     let (cxt_len, data) = try_read_u32(&data[id_len..])?;
     let cxt = &data[..cxt_len];
@@ -656,4 +651,20 @@ fn try_read_u32(data: &[u8]) -> Result<(usize, &[u8]), Vec<u8>> {
         .map_err(|_| Vec::new())
         .map(u32::from_be_bytes)?;
     Ok((res as usize, &data[4..]))
+}
+
+#[cfg(test)]
+mod test {
+    use hex::decode_to_slice;
+
+    use super::*;
+
+    #[cfg(feature = "k256")]
+    #[test]
+    fn test_hd_key_derive() {
+        let mut b = [0u8; 418];
+        decode_to_slice("0100000020fdb545b1b3d125d517148099c3403c7b1dd3f4cf3b012522afb7d12d07ba0b130000002b4c49545f48445f4b45595f49445f4b3235365f584d443a5348412d3235365f535357555f524f5f4e554c5f0000000a028506cbedca1d12788d6bc74627d99263c93204d2e9565d861b7c1270736b007102a89cb5090c0aaee9c5831df939abbeab2e0f62b5d54ceae6e816a9fe87c8ca32033e0c9d93b41414c3a8d287bb40ab024fbf176cb45c6616a3bf74e97bb68b516503a0c18f5d9db21fec597edef52f7a26449cdd90357532704a1ede6c27981a31b802794db35a0b6a6968ba4ed059630d788d591f083778dac9a45935549ca5f75ea603b398a663086dc7f1b5948d2195b176a7705fe71b0ad07110f57975254e6015980215f2cddeb89428f74132a84acf7e1a344f2ed9a39768f7006c9b8843e513dc550297d2a91f5a52e98873b7a4946c47d7736d6661cebace9c160d955999be97149203d2ee101c65ca0d60b5bc27ca1859c968984b1096d742874649bdc4fac6e9498a02bb0deb45aefb171e7117390991c2a230218fda04d9bb3cfd343f56ab61c3e390", &mut b).expect("Failed to decode hex");
+        let result = hd_key_derive(b.to_vec());
+        assert!(result.is_ok());
+    }
 }
--- src/core/src/utils.rs
+++
@@ -1,9 +0,0 @@
-pub(crate) fn read_4_byte_be(data: &[u8], offset: &mut usize) -> Result<u32, Vec<u8>> {
-    let value = u32::from_be_bytes(
-        data[*offset..*offset + 4]
-            .try_into()
-            .map_err(|_| Vec::new())?,
-    );
-    *offset += 4;
-    Ok(value)
-}
